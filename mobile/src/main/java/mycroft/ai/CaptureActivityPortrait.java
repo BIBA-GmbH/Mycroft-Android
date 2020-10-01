c428d582af918ac60811a3f0a69ccf7aac863174
abdulvahap <abdulvahap94@gmail.com>
2020-10-01 16:17:03 +0200

    Add a comment

--- mobile/src/main/java/mycroft/ai/CaptureActivityPortrait.java
+++ mobile/src/main/java/mycroft/ai/CaptureActivityPortrait.java
@@ -2,5 +2,6 @@ package mycroft.ai;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+// This class to have the scan feature in portrait mode which is set in manifest file.
 public class CaptureActivityPortrait extends CaptureActivity {
 }
